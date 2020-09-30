95b2846395304db599e5e3ad31121a6a25c08ac4
Igor Padovan da Silva <igorpadovan@gmail.com>
2020-09-29 21:28:07 -0400

    Initial version of SegurancaComunicacao generated by JHipster-6.10.1

---
+++ src/main/java/poc/pucminas/config/Constants.java
@@ -0,0 +1,12 @@
+package poc.pucminas.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM_ACCOUNT = "system";
+
+    private Constants() {
+    }
+}
---
+++ src/main/java/poc/pucminas/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package poc.pucminas.config.audit;
